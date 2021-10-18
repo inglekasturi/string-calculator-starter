a364e3203839504aa14e575929ba78602d608465
inglekasturi <91051405+inglekasturi@users.noreply.github.com>
2021-10-18 12:13:03 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,43 @@
 package calculator;
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+@Test
 class StringCalculatorShould {
+	
+	private StringCalculator stringCalculator;
+	
+	@BeforeTest
+	public void init() {
+		stringCalculator = new StringCalculator();
+	}
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+   
+   public void empty_string_should_return_0() throws Exception {
+        assertEquals(stringCalculator.add(""),0);   
     }
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+   public void string_with_single_number_should_return_number_as_int() throws Exception {
+       assertEquals(stringCalculator.add("1"),1);
+   }
+   public void twoNumbersCommaDelimitedReturnSum()throws Exception {
+   	assertEquals(stringCalculator.add("1,2"),3);
+   }
+   public void twoNumbersNewLineDelimitedReturnSum()throws Exception {
+   	assertEquals(stringCalculator.add("1\n2"),3);
+   }
+   public void threeNumbersDelimitedBothWayReturnSum() throws Exception{
+   	assertEquals(stringCalculator.add("1,2,3"),6);
+   }
+   public void multipleDelimiterReturnSum() throws Exception {
+   	assertEquals(stringCalculator.add("1;2"),3);
+   }
+   @Test(expectedExceptions=Exception.class )
+   public void  negativeInputReturnsException() throws Exception {
+   	stringCalculator.add("-1");
+   }
+   public void ignoreNumbersGreaterThan1000() throws Exception {
+   	assertEquals(stringCalculator.add("10,10,1001"),20);
+   }
 }
